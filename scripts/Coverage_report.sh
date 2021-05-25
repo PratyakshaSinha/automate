95d75bf3f9bd438ac8295b8080315e3b743bc841
Pratyaksha Sinha <65227203+PratyakshaSinha@users.noreply.github.com>
2021-05-25 16:41:02 +0530

    Update Coverage_report.sh

--- scripts/Coverage_report.sh
+++ scripts/Coverage_report.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
-
 #go to directory
-
-cd ../componnents/automate_ui
-
-# execute npm.test inside automate_ui
-
-$ exec npm test
-
+cd automate/componnents/automate-ui
+# execute npm test inside automate_ui
+exec npm test
 #list all the files
-$ exec ls
-
+exec ls
 #copy lcov.info 
- 
-srcdir="/automate/components/automate_ui/lcov.info"                   
+srcdir="/automate/components/automate-ui/lcov.info"                   
 dstdir="automate/Coverage_report/"
-source=/automate/components/automate_ui/lcov.info
+source=/automate/components/automate-ui/lcov.info
 destination=automate/Coverage_report/
 d=$(date +%m%d%y)
-
 if [ -f $source]; then
       dstfile=$(basename $source)
       cp "$source" "$dstdir/$dstfile"
